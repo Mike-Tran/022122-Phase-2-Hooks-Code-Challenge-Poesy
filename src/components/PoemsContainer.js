3dfab5d329ac00281e9fb67191f3f7de206db9cf
Marc Majcher <majcher@gmail.com>
2022-03-24 10:08:12 -0500

    init

---
+++ src/components/PoemsContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Poem from "./Poem";
+
+function PoemsContainer() {
+  return (
+    <div className="poems-container">
+      {/* render a list of <Poem> components in here */}
+    </div>
+  );
+}
+
+export default PoemsContainer;
